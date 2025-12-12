6f28a9c2e028cbb822509a948b59837ea8fad954
kazi tareq <kazitareq321@gmail.com>
2025-12-12 12:10:51 +0600

    change config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-    schema: "./prisma/schema.prisma",
+    schema: "./prisma/schema/schema.prisma",
   earlyAccess: true
 });
